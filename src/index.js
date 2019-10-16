570216c8a324e3b3efc0fca97e4bdeca92efe5b7
Dario <dkemenovic@gmail.com>
2019-10-16 19:07:48 +0200

    Refactor code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from "react-dom";
 import StorePicker from "./components/StorePicker"
 
-
 ReactDOM.render(
 	<StorePicker />,
 	document.getElementById('root')
