cca8129c58d69ea67edafbb649cdcebe7b290b56
Dario <dkemenovic@gmail.com>
2019-10-17 17:44:21 +0200

    Add event and method

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+	goToStore(event) {
+		event.preventDefault();
+		console.log("going to store")
+	}
 	render() {
 		return (
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 				<h2>Please enter a store</h2>
-				<input type="text" placeholder="Enter Name"/>
+				<input 
+					type="text" 
+					placeholder="Enter Name"
+				/>
 				<button type="submit">Visit Store!</button>
 			</form>
 		)
