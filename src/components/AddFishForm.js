80b9e4350ac6d228075e517a03e912516753b254
Dario <dkemenovic@gmail.com>
2019-10-18 20:48:56 +0200

    Create AddFishForm component

---
+++ src/components/AddFishForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AddFishForm extends React.Component {
+	render() {
+		return (
+			<form>
+        <p>It works</p>
+      </form>
+		)
+	}
+}
+
+export default AddFishForm;
+;
