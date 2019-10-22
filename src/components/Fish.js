f70cf2e475e209cc4eae1d6cbc71a9e2f0d8b752
Dario <dkemenovic@gmail.com>
2019-10-22 21:49:06 +0200

    Use ES6 destructuring

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -2,12 +2,12 @@ import React from "react";
 
 class Fish extends React.Component {
 	render() {
-		const image = this.props.details.image;
-		const name = this.props.details.name;
+		const {image, name, price, description, status} = this.props.details;
+
 		return (
 			<li className="menu-fish">
 				<img src={image} alt={name} />
-				<h3 className="fish-name">{this.props.details.name}</h3>
+				<h3 className="fish-name">{name}</h3>
 			</li>
 		);
 	}
