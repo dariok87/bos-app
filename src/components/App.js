cf38b7fc04ccadb6b0b4a3d048148d8be7eed930
Dario <dkemenovic@gmail.com>
2019-10-22 21:34:51 +0200

    Pass details object via props

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="FRESH SEAFOOD MARKET" />
           <ul className="fishes">
-            {Object.keys(this.state.fishes).map(key => <Fish key={key} />)}
+            {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]} />)}
           </ul>
         </div>
         <Order />
