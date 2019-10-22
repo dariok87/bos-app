8b7dded8245ab9f9bc586b1787ae57c3fc2d86d0
Dario <dkemenovic@gmail.com>
2019-10-22 22:00:20 +0200

    Import formatPrice function to get dollar price

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {formatPrice} from "../helpers";
 
 class Fish extends React.Component {
 	render() {
@@ -8,7 +9,7 @@ class Fish extends React.Component {
 			<li className="menu-fish">
 				<img src={image} alt={name} />
 				<h3 className="fish-name">{name}
-					<span className="price">{price}</span>
+					<span className="price">{formatPrice(price)}</span>
 				</h3>
 			</li>
 		);
