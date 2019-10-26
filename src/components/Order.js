29501f7b085121a00e2440de8f6e4c46725f7a4c
Dario <dkemenovic@gmail.com>
2019-10-26 21:30:57 +0200

    Add order Ids to Order

--- src/components/Order.js
+++ src/components/Order.js
@@ -2,7 +2,13 @@ import React from "react";
 
 class Order extends React.Component {
   render() {
-    return <div className="order-wrap">Order</div>;
+    const orderIds = Object.keys(this.props.order);
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        {orderIds}
+      </div>
+    );
   }
 }
 
