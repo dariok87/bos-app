60cc903c746c48e5bbae4bdd4f591e5263b517b5
Dario <dkemenovic@gmail.com>
2020-02-12 08:26:17 +0100

    Check if fish is available

--- src/components/Order.js
+++ src/components/Order.js
@@ -26,6 +26,9 @@ class Order extends React.Component {
       const fish = this.props.fishes[key];
       const count = this.props.order[key];
       const isAvailable = fish && fish.status === "available";
+      // make sure the fish is loaded before we continue
+      if (!fish) return null;
+
       if (isAvailable) {
         return prevTotal + count * fish.price;
       }
