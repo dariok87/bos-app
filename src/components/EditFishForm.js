c2c0435cb27eb7b4104dc5c0b219d68c69233f9b
Dario <dkemenovic@gmail.com>
2020-02-16 18:12:14 +0100

    Add div tag

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -2,7 +2,17 @@ import React from "react";
 
 class EditFishForm extends React.Component {
   render() {
-    return <p>Edit Fish</p>;
+    return (
+      <div>
+        <input type="text" name="name" />
+        <input type="text" name="price" />
+        <select type="text" name="status"></select>
+        <option value="available">Fresh!</option>
+        <option value="unavailable">Sold Out!</option>
+        <textarea type="text" name="desc" />
+        <input type="text" name="image" />
+      </div>
+    );
   }
 }
 
