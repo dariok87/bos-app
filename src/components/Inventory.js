c90545f6d6b657ac1856308292f3fb3d5ea135e0
Dario <dkemenovic@gmail.com>
2020-02-16 18:25:14 +0100

    Pass individaul fish down to EditFishForm

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -8,7 +8,7 @@ class Inventory extends React.Component {
       <div className="inventory">
         <h2>Inventory</h2>
         {Object.keys(this.props.fishes).map(key => (
-          <EditFishForm />
+          <EditFishForm fish={this.props.fishes[key]} />
         ))}
         <AddFishForm addFish={this.props.addFish} />
         <button onClick={this.props.loadSampleFishes}>
