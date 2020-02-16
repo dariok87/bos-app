512dbdf67db70317cfd9825a368d87c06a26f002
Dario <dkemenovic@gmail.com>
2020-02-16 18:45:24 +0100

    Add handleChange method

--- src/components/EditFishForm.js
+++ src/components/EditFishForm.js
@@ -1,17 +1,45 @@
 import React from "react";
 
 class EditFishForm extends React.Component {
+  handleChange = event => {
+    console.log(event);
+  };
   render() {
     return (
       <div>
-        <input type="text" name="name" value={this.props.fish.name} />
-        <input type="text" name="price" value={this.props.fish.price} />
-        <select type="text" name="status" value={this.props.fish.status}>
+        <input
+          type="text"
+          name="name"
+          onChange={this.handleChange}
+          value={this.props.fish.name}
+        />
+        <input
+          type="text"
+          name="price"
+          onChange={this.handleChange}
+          value={this.props.fish.price}
+        />
+        <select
+          type="text"
+          name="status"
+          onChange={this.handleChange}
+          value={this.props.fish.status}
+        >
           <option value="available">Fresh!</option>
           <option value="unavailable">Sold Out!</option>
         </select>
-        <textarea type="text" name="desc" value={this.props.fish.desc} />
-        <input type="text" name="image" value={this.props.fish.image} />
+        <textarea
+          type="text"
+          name="desc"
+          onChange={this.handleChange}
+          value={this.props.fish.desc}
+        />
+        <input
+          type="text"
+          name="image"
+          onChange={this.handleChange}
+          value={this.props.fish.image}
+        />
       </div>
     );
   }
