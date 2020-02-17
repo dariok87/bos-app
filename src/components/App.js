ce53351d5bc4b8a4c60893e1ea0d8c8cebfbcbf6
Dario <dkemenovic@gmail.com>
2020-02-17 19:16:21 +0100

    Create updateFish method

--- src/components/App.js
+++ src/components/App.js
@@ -46,6 +46,15 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  updateFish = (key, updateFish) => {
+    // 1. Take a copy of the current state
+    const fishes = { ...this.state.fishes };
+    // 2. update that state
+    fishes[key] = updateFish;
+    // 3. Set that to the state
+    this.setState({ fishes });
+  };
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes });
   };
