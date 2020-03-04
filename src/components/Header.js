ce0f5799f8e788dcfe1b7f289cfd2057e5d02b04
Dario <dkemenovic@gmail.com>
2020-03-04 22:03:53 +0100

    Add PropTypes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Header = props => (
-	<header className="top">
-		<h1>Catch of the day</h1>
-		<h3 className="tagline">
-		<span>{props.tagline}</span>
-		</h3>
-	</header>
-)
+  <header className="top">
+    <h1>Catch of the day</h1>
+    <h3 className="tagline">
+      <span>{props.tagline}</span>
+    </h3>
+  </header>
+);
+
+Header.propTypes = {
+  tagline: PropTypes.string.isRequired
+};
 
 export default Header;
