3be99560b536d8f6270cb4837c6a4d5521c3b5d9
Dario <dkemenovic@gmail.com>
2020-03-07 21:42:44 +0100

    Add props

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,11 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const Login = () => (
+const Login = props => (
   <nav className="login">
     <h2>Inventory Login</h2>
     <p>Sign in to your store's inventory.</p>
-    <button
-      className="facebook"
-      onClick={() => this.PaymentResponse.authenticate("Facebook")}
-    >
+    <button className="facebook" onClick={() => props.authenticate("Facebook")}>
       Log In With Facebook
     </button>
   </nav>
