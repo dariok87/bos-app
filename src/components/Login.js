6786174280ed0b1e9c72a42a419fa6ff68bd10f7
Dario <dkemenovic@gmail.com>
2020-03-07 21:33:58 +0100

    Add Facebook authentication

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,13 @@ import React from "react";
 const Login = () => (
   <nav className="login">
     <h2>Inventory Login</h2>
+    <p>Sign in to your store's inventory.</p>
+    <button
+      className="facebook"
+      onClick={() => this.PaymentResponse.authenticate("Facebook")}
+    >
+      Log In With Facebook
+    </button>
   </nav>
 );
 
